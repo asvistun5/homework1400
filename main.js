76fc971f39a230c8769baef9bf028afe5b1d7658
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2025-09-17 21:21:05 +0300

    create express server

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const moment = require('moment');
+const express = require('express');
+const app = express();
 
 function getDate() {
-    console.log(moment().format('YYYY/DD/MMHH:mm:ss'))
+    return moment().format('YYYY-MM-DD HH:mm:ss');
 }
 
-function getDay() {
-    console.log(moment().format('dddd'))
-}
+app.get('/timestamp', (req, res) => {
+    res.json({ timestamp: getDate() });
+});
 
-getDate();
-getDay();
+app.listen(8000, 'localhost', () => {
+    console.log('https://localhost:8000');
+});
