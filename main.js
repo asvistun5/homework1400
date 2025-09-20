b8c78d5e8df702f498f4d1b51cebca7210f042bf
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2025-09-20 19:46:05 +0300

    add posts endpoint

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
+
+const prodPath = path.join(__dirname, "posts.json");
+const prodJson = JSON.parse(fs.readFileSync(prodPath, 'UTF-8'));
 
 function getDate() {
     return moment().format('YYYY-MM-DD HH:mm:ss');
 }
 
 app.get('/timestamp', (req, res) => {
-    res.json({ timestamp: getDate() });
+    res.json({ 
+        timestamp: getDate() 
+    });
+});
+
+app.get('/posts', (req, res) => {
+    res.json({ 
+        posts: prodJson
+    });
 });
 
 app.listen(8000, 'localhost', () => {
-    console.log('https://localhost:8000');
+    console.log('http://localhost:8000');
 });
