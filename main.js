15ea0cea0c924cfc0df73d951d790c088ffd3ab9
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2025-09-26 21:44:14 +0300

    create find function with take and skip

--- main.js
+++ main.js
@@ -19,9 +19,41 @@ app.get('/timestamp', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
-    res.json({ 
-        posts: prodJson
+    let skip = 0;
+    let take = prodJson.length;
+
+    if (req.query.skip !== undefined) {
+        if (isNaN(req.query.skip)) {
+            res.status(400).json({ error: "'skip' isnt a number" });
+            return;
+        }
+        skip = parseInt(req.query.skip);
+    }
+
+    if (req.query.take !== undefined) {
+        if (isNaN(req.query.take)) {
+            res.status(400).json({ error: "'take' isnt a number" });
+            return;
+        }
+        take = parseInt(req.query.take);
+    }
+
+    const selectedPosts = prodJson.slice(skip, skip + take);
+    res.json({ posts: selectedPosts });
+});
+
+app.get('/posts/:id', (req, res) => {
+    const postId = req.params.id;
+    const found = prodJson.find((post) => {
+        return String(post.id) === postId;
     });
+
+    if (found === undefined || found === null) {
+        res.status(404).json({ error: "404 not found" });
+        return;
+    }
+
+    res.json(found);
 });
 
 app.listen(8000, 'localhost', () => {
