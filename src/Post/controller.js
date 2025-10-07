1a275209a4586f94fe35b9d2663808e4e61278c1
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2025-10-07 19:18:22 +0300

    fix async

--- src/Post/controller.js
+++ src/Post/controller.js
@@ -7,23 +7,17 @@ const postController = {
         res.json({ timestamp: moment().format('YYYY-MM-DD HH:mm:ss') });
     },
 
-    async getAll(req, res) {
-        try {
-            const posts = await postService.getAll(req.query.skip, req.query.take);
-            res.json({ posts });
-        } catch (e) {
-            res.status(500).json({ error: 'Error reading posts file' });
-        }
+    getAll(req, res) {
+        const posts = postService.getAll(req.query.skip, req.query.take);
+        res.json({ posts });
+        res.status(500).json({ error: 'Error reading posts file' });
     },
 
-    async getById(req, res) {
-        try {
-            const post = await postService.getById(req.params.id);
-            if (!post) return res.status(404).json({ error: 'Not found' });
-            res.json(post);
-        } catch {
-            res.status(500).json({ error: 'Error reading posts file' });
-        }
+    getById(req, res) {
+        const post = postService.getById(req.params.id);
+        if (!post) return res.status(404).json({ error: 'Not found' });
+        res.json(post);
+        res.status(500).json({ error: 'Error reading posts file' });
     },
 
     async create(req, res) {
