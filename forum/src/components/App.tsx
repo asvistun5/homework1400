1f4ad19760655d405bb37576bca98b02dc613175
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2025-11-26 22:13:13 +0200

    update main page

--- forum/src/components/App.tsx
+++ forum/src/components/App.tsx
@@ -1,24 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles.css";
 import gitIcon from "../assets/icons/git.svg";
-import 'material-icons/iconfont/material-icons.css';
+import postsIco from "../assets/icons/posts.svg"
+import Posts from "../pages/Posts";
 
 
 export default function App() {
     return (
-        <div>
-            <Nav/>
-            <main>
-                <h1>Ласкаво просимо на наш сайт!</h1>
-                <p>Forum — платформа для обміну<br/>цікавою інформацією</p>
-            </main>
-            <footer>
-                <p>© 2025 Forum App</p>
-                <div>
-                    <img src={gitIcon}/>
-                    <a href="https://github.com/asvistun5/" target="__blank">Автор: Artem Svistun</a>
-                </div>
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Nav/>
+                <main>
+                    <h1>Ласкаво просимо на наш сайт!</h1>
+                    <p>Forum — платформа для обміну<br/>цікавою інформацією</p>
+                </main>
+                <footer>
+                    <p>© 2025 Forum App</p>
+                    <div>
+                        <img src={gitIcon}/>
+                        <a href="https://github.com/asvistun5/" target="__blank">Автор: Artem Svistun</a>
+                    </div>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
@@ -27,7 +31,7 @@ export function Nav() {
         <nav>
             <h1>Forum</h1>
             <div>
-                <button className="posts-btn"><span className="material-icons">email</span></button>
+                <button className="posts-btn"><img src={postsIco}/></button>
                 <button className="signup">Реєстрація</button>
                 <button className="signin">Увійти</button>
             </div>
---
+++ forum/src/pages/Posts.tsx
@@ -0,0 +1,14 @@
+export default function Posts() {
+    return (
+        <main>
+            <div className="searchbox">
+                <input type="text" placeholder="Пошук"/>
+            </div>
+            <div className="tags">
+                <div>#tag</div>
+                <div>#tag2</div>
+                <div>#tag3</div>
+            </div>
+        </main>
+    );
+}
