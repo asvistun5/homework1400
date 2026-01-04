c06fbbca5a5decd34b3d9bbd0c548170dcc5160a
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2026-01-04 22:32:44 +0200

    add cors

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from "cors";
+
 import postRouter from './backend/Post/router';
 import tagRouter from './backend/Tags/router';
 import userRouter from './backend/User/router';
 
 const app: express.Express = express()
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"]
+}))
 
 app.use(express.json());
 app.use('/posts', postRouter);
